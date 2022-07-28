0c102e4d69363db8a3b4e4b4e597b334cf8f028c
Jhorner22 <jphorner22@gmail.com>
2022-07-28 05:04:49 -0500

    add repos from github to db route

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 from flask import Flask
 import pymongo
 from pymongo.server_api import ServerApi
+import requests
+from types import SimpleNamespace
 
 app = Flask(__name__)
 
@@ -15,6 +17,15 @@ def testadd():
     db.repos.tests.insert_one({'test':'test1'})
     return "added"
 
+@app.route('/addrepos')
+def addrepos():
+    db = dbconnect()
+    url = "https://api.github.com/users/Jhorner22/repos"
+    resp = requests.get(url=url).json()
+    db.github_repos.insert_many(resp)
+    return f"added {resp}"
+
+    
 def dbconnect():
     print("connecting")
     password =  "!password123"
