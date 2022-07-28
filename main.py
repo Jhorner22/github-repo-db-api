267574750478364ea5d09ab1e31e4686b1e1c2de
Jhorner22 <jphorner22@gmail.com>
2022-07-28 04:35:57 -0500

    db connection

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 from flask import Flask
+import pymongo
+from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
 
@@ -7,4 +9,21 @@ app = Flask(__name__)
 def index():
     return json.dumps({'hello':'world'})
 
-app.run()
+@app.route('/testadd')
+def testadd():
+    db = dbconnect()
+    db.repos.tests.insert_one({'test':'test1'})
+    return "added"
+
+def dbconnect():
+    print("connecting")
+    password =  "!password123"
+    client = pymongo.MongoClient(f"mongodb+srv://user2:{password}@cluster0.qfxug.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+    db = client.test
+    return db
+
+    
+
+
+if __name__ == '__main__':
+    app.run()
